0c25eb08b5d260e4066db16527bf289d0acc3859
Preyash <68819672+preyasH61@users.noreply.github.com>
2021-06-02 12:31:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBj-wefkeGkdSS8MbxbwdT7oUW3c9eKagY",
-    authDomain: "whatsapp-cl-f361d.firebaseapp.com",
-    projectId: "whatsapp-cl-f361d",
-    storageBucket: "whatsapp-cl-f361d.appspot.com",
-    messagingSenderId: "744115326935",
-    appId: "1:744115326935:web:c2da2c350defdbcbb08592",
-    measurementId: "G-WHVFVDHKWY"
-  };
+past your fiebase config code
